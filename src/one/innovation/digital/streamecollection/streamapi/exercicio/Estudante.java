58ccbdde8f6f61e4dfceee4ed441fe15bc91cb24
LuizHViras <luizhviras@uol.com.br>
2021-08-31 18:15:24 -0300

    Santander Bootcamp Fullstack Developer - DIO

---
+++ src/one/innovation/digital/streamecollection/streamapi/exercicio/Estudante.java
@@ -0,0 +1,27 @@
+package one.innovation.digital.streamecollection.streamapi.exercicio;
+
+public class Estudante {
+
+    private final String nome;
+    private final Integer idade;
+
+
+    public Estudante(String nome, Integer idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    @Override
+    public String toString() {
+        return "nome='" + nome + '\'' + ", idade=" + idade;
+    }
+}
+
